98599567d943b4455dffc0527a83eb074118ad00
NimishRathi <nimish.rathi2002@gmail.com>
2025-01-07 13:13:16 +0530

    my first java program

---
+++ welcome.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class welcome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("welcome to java ");
+        
+    }
+}
